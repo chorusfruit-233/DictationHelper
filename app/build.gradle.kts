c4e0b4e3b5e4296944ff8e14462d9c96cc0a7c30
chorusfruit <564005180@qq.com>
2026-09-13 12:26:32 +0800

    Bump version to 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.example.dictationhelper"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
