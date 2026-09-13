dc876e46cedfee7c94bb8d33a23b16de79fda038
chorusfruit <564005180@qq.com>
2026-09-13 15:08:05 +0800

    Bump version to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.example.dictationhelper"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
